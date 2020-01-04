6d46b6569b450651de89faaef1912b9878b18cb8
Steven Hankin <steven.hankin@gmail.com>
2020-01-04 17:47:49 +0000

    feat: car and booking dynamodb tables

---
+++ service/src/lambda/http/getCars.ts
@@ -0,0 +1,13 @@
+import { APIGatewayProxyHandler,APIGatewayProxyResult } from 'aws-lambda';
+
+import 'source-map-support/register';
+
+export const hello: APIGatewayProxyHandler = async (event, _context): Promise<APIGatewayProxyResult> => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      input: event,
+    }, null, 2),
+  };
+}
